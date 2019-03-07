13c4ff2031cba0fff2942e11069796fead6ed376
Ryan Comerford <ryncmrfrd@users.noreply.github.com>
2019-03-07 14:20:42 +1100

    y

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
-async function auth(){
-    await googleTasksApi.authorize('248150601049-fbibbrvjeqojdj45csgilhmj2vk7240e.apps.googleusercontent.com');
-}
-auth();
+await googleTasksAPI.autorize('yourclientid')
