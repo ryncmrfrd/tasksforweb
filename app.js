1d28fa8f31a23a2cded9f13009bfdd72f715d12d
Ryan Comerford <ryncmrfrd@users.noreply.github.com>
2019-03-07 13:56:19 +1100

    y

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-googleTasksApi.authorize('248150601049-fbibbrvjeqojdj45csgilhmj2vk7240e.apps.googleusercontent.com');
+async function auth(){
+    await googleTasksApi.authorize('248150601049-fbibbrvjeqojdj45csgilhmj2vk7240e.apps.googleusercontent.com');
+}
+auth();
