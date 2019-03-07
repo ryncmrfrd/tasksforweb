91fae2c00fc79ae013e11fe834c7dcdc139af453
Ryan Comerford <ryncmrfrd@users.noreply.github.com>
2019-03-07 14:50:34 +1100

    yeet

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-googleTasksApi.authorize('248150601049-fbibbrvjeqojdj45csgilhmj2vk7240e.apps.googleusercontent.com');
+googleTasksApi.authorize('248150601049-fbibbrvjeqojdj45csgilhmj2vk7240e.apps.googleusercontent.com', 'https://ryncmrfrd.me');
 googleTasksApi.signIn();
