cf1b79ce7918e36e389ab30dfeb2730756e90f44
Ryan Comerford <40415962+ryncmrfrd@users.noreply.github.com>
2019-03-07 21:50:50 +1100

    y

--- app.js
+++ app.js
@@ -15,72 +15,43 @@
       /**
        *  On load, called to load the auth2 library and API client library.
        */
-      function handleClientLoad() {
-        gapi.load('client:auth2', initClient);
-      }
+      function clientLoad() {
+        gapi.load('client:auth2', function(){
+          gapi.client.init({
+            apiKey: API_KEY,
+            clientId: CLIENT_ID,
+            discoveryDocs: DISCOVERY_DOCS,
+            scope: SCOPES
+          }).then(function(){
+            // Listen for sign-in state changes.
+            gapi.auth2.getAuthInstance().isSignedIn.listen(function(){
+              if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
+                authorizeButton.style.display = 'none';
+                signoutButton.style.display = 'block';
+                listTaskLists();
+              } else {
+                authorizeButton.style.display = 'block';
+                signoutButton.style.display = 'none';
+              }
+            });
 
-      /**
-       *  Initializes the API client library and sets up sign-in state
-       *  listeners.
-       */
-      function initClient() {
-        gapi.client.init({
-          apiKey: API_KEY,
-          clientId: CLIENT_ID,
-          discoveryDocs: DISCOVERY_DOCS,
-          scope: SCOPES
-        }).then(function () {
-          // Listen for sign-in state changes.
-          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-          // Handle the initial sign-in state.
-          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-          authorizeButton.onclick = handleAuthClick;
-          signoutButton.onclick = handleSignoutClick;
-        }, function(error) {
-          appendPre(JSON.stringify(error, null, 2));
+            authorizeButton.onclick = handleAuthClick;
+            signoutButton.onclick = handleSignoutClick;
+          });
         });
-      }
-
-      /**
-       *  Called when the signed in status changes, to update the UI
-       *  appropriately. After a sign-in, the API is called.
-       */
-      function updateSigninStatus(isSignedIn) {
-        if (isSignedIn) {
-          authorizeButton.style.display = 'none';
-          signoutButton.style.display = 'block';
-          listTaskLists();
-        } else {
-          authorizeButton.style.display = 'block';
-          signoutButton.style.display = 'none';
-        }
       }
 
       /**
        *  Sign in the user upon button click.
        */
-      function handleAuthClick(event) {
+      function signIn() {
         gapi.auth2.getAuthInstance().signIn();
       }
-
       /**
        *  Sign out the user upon button click.
        */
-      function handleSignoutClick(event) {
-        gapi.auth2.getAuthInstance().signOut();
-      }
-
-      /**
-       * Append a pre element to the body containing the given message
-       * as its text node. Used to display the results of the API call.
-       *
-       * @param {string} message Text to be placed in pre element.
-       */
-      function appendPre(message) {
-        var pre = document.getElementById('content');
-        var textContent = document.createTextNode(message + '\n');
-        pre.appendChild(textContent);
+      function signOut() {
+gapi.auth2.getAuthInstance().signOut();
       }
 
       /**
@@ -90,15 +61,15 @@
         gapi.client.tasks.tasklists.list({
             'maxResults': 10
         }).then(function(response) {
-          appendPre('Task Lists:');
+          console.log('Task Lists:');
           var taskLists = response.result.items;
           if (taskLists && taskLists.length > 0) {
             for (var i = 0; i < taskLists.length; i++) {
               var taskList = taskLists[i];
-              appendPre(taskList.title + ' (' + taskList.id + ')');
+              console.log(taskList.title + ' (' + taskList.id + ')');
             }
-          } else {
-            appendPre('No task lists found.');
+          }else {
+            console.log('No task lists found.');
           }
         });
       }
