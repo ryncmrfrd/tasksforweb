56af6ef8b7b7c787c53767263df11dac17f5c262
Ryan Comerford <40415962+ryncmrfrd@users.noreply.github.com>
2019-03-08 17:46:46 +1100

    mmm nah

--- tasksAPI.js
+++ tasksAPI.js
@@ -1,6 +1,6 @@
 'use strict';
 var tasks = {
-    //init
+    //[TESTED WORKING]
     initialize: function(key, client){
         if(!key || !client){return false}
         gapi.load('client:auth2', function(){
@@ -22,19 +22,21 @@ var tasks = {
             });
         });
     },
-    //oauth
+    //[TESTED WORKING]
     signIn: function(){
       gapi.auth2.getAuthInstance().signIn();
     },
+    //[TESTED WORKING]
     signOut: function(){
       gapi.auth2.getAuthInstance().signOut();
     },
+    //[TESTED WORKING]
     isSignedIn: function(){
       return gapi.auth2.getAuthInstance().isSignedIn.get();
     },
-    //task lists
     getTaskLists: function(){
       gapi.client.tasks.tasklists.list().then(function(response) {
+        console.log(response);
         return response;
       });
     },
@@ -48,10 +50,10 @@ var tasks = {
         tasklist: listID
       });
     },
-    //tasks
     getTasks: function(taskListId){
-      return gapi.client.tasks.tasks.list({
-        tasklist: taskListId
+      gapi.client.tasks.tasks.list({tasklist: taskListId}).then(function(response) {
+        console.log(response);
+        return response;
       });
     },
     addTask: function(taskListID, title, notes){
