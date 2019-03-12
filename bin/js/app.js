7069ae05ea71304e0d398941412f512035d85f22
Ryan Comerford <ryncmrfrd@users.noreply.github.com>
2019-03-12 19:48:12 +1100

    YGHU

--- bin/js/app.js
+++ bin/js/app.js
@@ -1,46 +1,61 @@
 var currentList;
 $('#task-list-title').change(function (){
+    updateTaskLists()
+    $('#task-wrapper section').hide();
+    $('#task-wrapper section.'+currentList).show();
     currentList = $(this).val();
 });
 
 function startApp(){
-    if(!humanTasks.auth.isLoggedIn()){humanTasks.auth.login(taskLists());}
-    else{taskLists()}
-    function taskLists(){
+    if(!humanTasks.auth.isLoggedIn()){humanTasks.auth.login(getTaskLists());}
+    else{getTaskLists()}
+    function getTaskLists(){
         humanTasks.taskLists.get(function(taskLists){
             for(i = 0; i < taskLists.length; i++){
-                $('#task-list-title').append(
-                    '<option value="'+taskLists[i].id+'">'+taskLists[i].title+'</option>'
-                );
+                $('#task-list-title').append('<option value="'+taskLists[i].id+'">'+taskLists[i].title+'</option>');
+                $('#task-wrapper').append('<section class="'+taskLists[i].id+'"></section>');
             }
             currentList = $('#task-list-title').val();
-            humanTasks.tasks.get(currentList, function(tasks){
-                for(i = 0; i < tasks.length; i++){
-                    if(tasks[i].status == 'needsAction'){
-                        $('#task-wrapper').append(
-                            '<div class="task" id="'+tasks[i].id+'">'+
-                                '<button onclick="setElementAsCompleted(this)" id="task-button"><i class="fas fa-check"></i></button>'+
-                                '<div class="task-details">'+
-                                    '<h2 class="task-title">'+tasks[i].title+'</h2>'+
-                                '</div>'+
-                            '</div>'
-                        );
-                    }
-                }
-            })
+            updateTaskLists();
         });
-        
+    }  
+}
+
+function updateTaskLists(){
+    for(var i = 0; i < $('#task-list-title option').length; i++){
+        var taskListID = $('#task-list-title option').children().prevObject[i].value;
+        humanTasks.tasks.get(taskListID, function(tasks){
+            if(tasks==undefined){return;}
+            $('#task-wrapper section').empty();
+            for(i = 0; i < tasks.length; i++){
+                if(tasks[i].status == 'needsAction'){
+                    $('#task-wrapper section.'+taskListID).append(
+                        '<div class="task" id="'+tasks[i].id+'">'+
+                            '<button onclick="setElementAsCompleted(this)" id="task-button"><i class="fas fa-check"></i></button>'+
+                            '<div class="task-details">'+
+                                '<h2 contenteditable="true" class="task-title">'+tasks[i].title+'</h2>'+
+                            '</div>'+
+                        '</div>'
+                    );
+                }
+            }
+            allowTaskEdits();
+        })
     }
 }
 
-function setElementAsCompleted(x){
-    var params = {
-        "status": "completed"
-    }
-    humanTasks.tasks.edit(currentList, x.parentElement.id, params, function(response){
-        console.log(
-                   //   x.parentElement.parentElement.removeChild()
-        )
-
+function allowTaskEdits(){
+    $('.task-title').focusout(function(){
+        var taskID = this.parentNode.parentNode.id;
+        var params = {'title' : this.innerText}
+        humanTasks.tasks.edit(currentList, taskID, params)
     });
+}
+
+function setElementAsCompleted(x){
+    console.log('TaskList - '+currentList)
+    var thisID = x.parentNode.id;
+    humanTasks.tasks.complete(currentList, x.parentNode.id,function(x){
+        $('#'+skrrt).remove();
+    })
 }
